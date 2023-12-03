91f0b0b7e663c671f7238c76b93396ffd6f5662f
German Sarachu <germansarachubrusco@hotmail.com>
2023-12-03 17:26:20 -0300

    cambio galerias y agrego scss

--- scripts.js
+++
@@ -1,4 +0,0 @@
-  function toggleGallery(galleryId) {
-    var gallery = document.getElementById(galleryId);
-    gallery.style.display = (gallery.style.display === 'none' || gallery.style.display === '') ? 'block' : 'none';
-  }
